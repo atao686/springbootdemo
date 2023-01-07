2882e3bd01f50793445e7c2043ff96cbc06d25bc
atao <zts_75@163.com>
2023-01-07 22:15:10 +0800

    Initial commit

---
+++ src/main/java/com/atao/algorithm/TreeNode.java
@@ -0,0 +1,21 @@
+package com.atao.algorithm;
+
+/**
+ * @Description 树结点的定义
+ * @Author atao
+ */
+public class TreeNode {
+
+    public int val;
+    public TreeNode left;
+    public TreeNode right;
+    public  TreeNode() {}
+    public  TreeNode(int val)
+    { this.val = val; }
+    public  TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+
+}
